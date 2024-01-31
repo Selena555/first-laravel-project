99b4b77c98a894e369c2bfbc31f7a3757fdde91c
Selena555 <ko4kareva.elena@yandex.ru>
2024-01-31 21:48:12 +0300

    Реквест для категорий и форма создания категорий

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,14 @@
+<form action="{{'categories.store'}}" method="post" enctype="multipart/form-data">
+    @csrf
+    <label>
+        Название
+        <input type="text" name="name" placeholder="название поста" required>
+    </label>
+    <label>
+        Постер
+        <input type="file" name="poster" id="poster" required>
+    </label>
+    <button type="submit">
+        <span>Сохранить</span>
+    </button>
+</form>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <label>
         Название
-        <input type="text" name="name" placeholder="название поста">
+        <input type="text" name="name" placeholder="название поста" required>
     </label>
 
     <label>
@@ -12,12 +12,21 @@
 
     <label>
         Содержимое
-        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        <textarea name="content" id="content" cols="30" rows="10" required></textarea>
     </label>
 
     <label>
         Постер
-        <input type="file" name="poster" id="poster">
+        <input type="file" name="poster" id="poster" required>
+    </label>
+
+    <label>
+        Категория
+        <select name="category_ids" id="category_ids" multiple>
+           @foreach($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+        </select>
     </label>
 
     <button type="submit">
