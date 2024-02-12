f9f8bd552148cde3a77e7a8f7c29d524a2abb439
Selena555 <ko4kareva.elena@yandex.ru>
2024-02-12 16:21:19 +0300

    Стилизация форм

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,15 +1,43 @@
-<form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
-    @csrf
-    <label>
-        Название
-        <input type="text" name="name" placeholder="название поста" required>
-    </label>
-    <label>
-        Постер
-        <input type="file" name="poster" id="poster" required>
-    </label>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="container">
+    <div class="login-box">
+        <h1 class="title">Создание категории</h1>
+        <form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="user-box">
+                <label>
+                    <span>Название</span>
+                    <input type="text" name="name" placeholder="Название поста" required/>
+                </label>
+            </div>
+            <div class="user-box">
+                <label>
+                    <span>Постер</span>
+                    <input type="file" name="poster" id="poster" required/>
+                </label>
+            </div>
 
-    <button type="submit">
-        <span>Сохранить</span>
-    </button>
-</form>
+            <button class="btn" type="submit">
+                <span>Сохранить</span>
+            </button>
+        </form>
+    </div>
+</div>
+</body>
+
+</html>
+
+
+
+
+
+
+
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,35 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="container">
+    <div class="login-box">
+        <h1 class="title">Создание поста</h1>
 <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
     @csrf
+    <div class="user-box">
     <label>
-        Название
-        <input type="text" name="name" placeholder="название поста" required>
+        <span>Название</span>
+        <input type="text" name="name" placeholder="Название поста" required>
     </label>
-
+    </div>
+    <div class="user-box">
     <label>
-        Описание
-        <input type="text" name="description" placeholder="описание поста">
+        <span>Описание</span>
+        <input type="text" name="description" placeholder="Описание поста">
     </label>
-
+    </div>
+    <div class="user-box">
     <label>
-        Содержимое
-        <textarea name="content" id="content" cols="30" rows="10" required></textarea>
+        <span>Содержимое</span>
+        <textarea style="width: 311px; height: 158px; margin-bottom: 35px;" name="content" id="content" cols="30" rows="10" required></textarea>
     </label>
-
+    </div>
+    <div class="user-box">
     <label>
-        Постер
+        <span>Постер</span>
         <input type="file" name="poster" id="poster" required>
     </label>
-
+    </div>
+    <div class="user-box">
     <label>
-        Категория
+        <span>Категория</span>
         <select name="category_ids" id="category_ids" multiple>
            @foreach($categories as $category)
                 <option value="{{ $category->id }}">{{ $category->name }}</option>
             @endforeach
         </select>
     </label>
-
-    <button type="submit">
+    </div>
+    <button class="btn" type="submit">
         <span>Сохранить</span>
     </button>
 </form>
+    </div>
+</div>
+</body>
+
+</html>
