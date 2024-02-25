e240c9a9af7cd0101ee282b9fdf33187c240df89
Selena555 <ko4kareva.elena@yandex.ru>
2024-02-25 17:46:12 +0300

    Оформила реактивность для постов

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/style-post.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style-post.css')}}">
 </head>
 <body>
 <div class="container">
