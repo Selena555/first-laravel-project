7a590bfdc4e3ac79bd417013675581548f9f000d
Selena555 <ko4kareva.elena@yandex.ru>
2024-03-25 21:50:06 +0300

    вернули роуты и защитили их авторизацией

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('comments', CommentController::class)->only(['store', 'destroy']);
 });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('posts', PostController::class)->only(['create','store','destroy']);
+    Route::resource('categories', CategoryController::class)->only(['create','store','destroy']);
+});
+
+Route::resource('posts', PostController::class)->only(['index','show']);
+Route::resource('categories', CategoryController::class)->only(['index','show']);
+
 
 require __DIR__.'/auth.php';
